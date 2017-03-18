2ef8a65f0e76bfe6cfed3ac0fa8c9657726e8dfd
jessewriter <jesse_test@me.com>
2017-03-18 12:53:23 -0700

    initial commit

---
+++ app/src/main/java/com/iot/jesseboyd/buttonspractice/MainActivity.java
@@ -0,0 +1,91 @@
+package com.iot.jesseboyd.buttonspractice;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tv = (TextView) findViewById(R.id.textView);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Snackbar.make(view, "Replace with your own action (will open https://www.linkedin.com/in/mrjesseboyd)", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                //delay the intent
+                Handler mHandler = new Handler();
+                mHandler.postDelayed(mUpdateTimeTask, 1000);
+
+
+            }
+
+            private Runnable mUpdateTimeTask = new Runnable() {
+                public void run() {
+                    // do what you need to do here after the delay
+                    //on click of the envelope icon open my LinkedIn page
+                    Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.linkedin.com/in/mrjesseboyd"));
+                    startActivity(intent);
+
+                }
+            };
+        });
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    tv.setText("toggle is checked");
+                } else {
+                    tv.setText("toggle is unchecked");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
